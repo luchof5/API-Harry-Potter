a86cd726b02c302f23e63cc1115dd4b15d1a2291
LuchoF5 <you@example.com>
2024-06-07 23:44:43 -0300

    js add: character filtering

--- app/main.js
+++ app/main.js
@@ -5,19 +5,22 @@ const url = 'https://hp-api.onrender.com/api/characters';
 fetch(url)
     .then(response => response.json())
     .then(data => showData(data));
+    
 
 function showData(data) {
     console.log(data);
-   
     data.forEach(character => { 
-        if(character.image !== '') {
-        $divCards.innerHTML += `<div class="card">
-        <h4>${character.name}</h4>
-        <img class="imgCards" src="${character.image}" alt="imagen de ${character.name}">
-        <p>Casa: ${character.house}</p>
-        <p>Actor: ${character.actor}</p>
-        </div>
-        `;
+        if(!character.image) {
+            character.image = '../public/perfil.png'
         }
+        if(character.house) {
+            $divCards.innerHTML += `<div class="card">
+            <h4>${character.name}</h4>
+            <img class="imgCards" src="${character.image}" alt="imagen de ${character.name}">
+            <p>Casa: ${character.house}</p>
+            </div>
+            `;
+        }
+        
     }); 
 };
